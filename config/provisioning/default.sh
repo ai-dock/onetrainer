1899127fdf903ceed400a50577f095748a3fa221
Rob Ballantyne <rob@dynamedia.uk>
2024-03-11 16:48:52 +0000

    No action in default provisioning script [skip ci]

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -12,7 +12,7 @@ PYTHON_PACKAGES=(
 
 
 CHECKPOINT_MODELS=(
-    "https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
+    #"https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
     #"https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
